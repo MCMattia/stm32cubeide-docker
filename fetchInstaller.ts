3783bdf9500a7515cf2cb8e364e861031c7154ba
MCMattia <mcemail@posteo.ch>
2024-10-24 08:33:27 +0200

    fix: fetch correct installer

--- fetchInstaller.ts
+++ fetchInstaller.ts
@@ -1,9 +1,9 @@
 // deno run --allow-net --allow-write fetchInstaller.ts
 
 const url =
-  "https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group1/16/f0/7b/d1/a5/6d/42/66/stm32cubeide-lnx/files/st-stm32cubeide_1.16.1_22882_20240916_0822_amd64.sh.zip/jcr:content/translations/en.st-stm32cubeide_1.16.1_22882_20240916_0822_amd64.sh.zip";
+  "https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group1/6e/81/da/02/ec/61/46/ab/stm32cubeide-deb/files/st-stm32cubeide_1.16.1_22882_20240916_0822_amd64.deb_bundle.sh.zip/jcr:content/translations/en.st-stm32cubeide_1.16.1_22882_20240916_0822_amd64.deb_bundle.sh.zip";
 const filename =
-  "./en.st-stm32cubeide_1.16.1_22882_20240916_0822_amd64.sh.zip";
+  "./en.st-stm32cubeide_1.16.1_22882_20240916_0822_amd64.deb_bundle.sh.zip";
 
 async function download(url: string, filename: string) {
   const response = await fetch(url);
